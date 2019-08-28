deec503e64a9077365cba3e1c84d0fab0c973e7e
David Strauch <dmaster18@gmail.com>
2019-08-28 19:58:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,12 +2,16 @@
 class School
   def initialize (school)
     @school = school
+<<<<<<< HEAD
     @roster = {}
+=======
+>>>>>>> 4f67616252ca0640a33a75b166979bff03d813fb
   end
   
   def add_student(student, grade)
     @student = student
     @grade = grade
+<<<<<<< HEAD
     if @roster.has_key?(@grade) == true
       @roster[@grade] << @student
     else 
@@ -18,10 +22,16 @@ class School
   
   def roster
     @roster
+=======
+    roster[@grade] = []
+    roster[@grade] << @student
+    puts roster[@grade]
+>>>>>>> 4f67616252ca0640a33a75b166979bff03d813fb
   end
   
   def grade(grade)
     @grade = grade
+<<<<<<< HEAD
     @roster[@grade]
   end
   
@@ -44,5 +54,21 @@ puts school.roster
 puts school.grade(9)
 puts school.sort
 
+=======
+    roster[grade]
+  end
+  
+  def roster
+    roster = {
+    }
+  end
+end
+school = School.new("Bayside High School")
+puts school.roster
+school.add_student("AC Slater", "9")
+school.add_student("Kelly Kapowski", "10")
+school.add_student("Screech", "11")
+puts school.roster
+>>>>>>> 4f67616252ca0640a33a75b166979bff03d813fb
 
 
---
+++ school.rb
@@ -0,0 +1,14 @@
+class School
+  ROSTER = []
+  def initialize(school_name)
+    @school_name = school_name
+  end
+  
+  def new_student(student_name)
+    @student_name = student_name    
+    ROSTER << student_name
+  end
+  
+  def roster
+    puts "There are #{ROSTER}.length students in #{@school_name}."
+end
