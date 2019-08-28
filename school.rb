f14fe8b9683dd9d274bd9c4cb545df3e82fe22e5
David Strauch <dmaster18@gmail.com>
2019-08-28 20:05:27 +0000

    Automatically backed up by Learn

--- school.rb
+++ school.rb
@@ -25,6 +25,6 @@ scarsdale.new_student("Joe", 9)
 scarsdale.new_student("Alan", 10)
 scarsdale.new_student("Bernice", 11)
 scarsdale.new_student("Lamar", 9)
-scarsdale.new_student("Chaim")
-scarsdale.new_student("Mohammed")
+scarsdale.new_student("Chaim", 11)
+scarsdale.new_student("Mohammed", 12)
 scarsdale.roster
